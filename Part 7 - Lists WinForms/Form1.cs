47854967f025a9454930f75b7e4719a011753090
Evan Geurts <GeurEvan189@GoTVDSB.ca>
2021-03-19 10:01:25 -0400

    Add project files.

---
+++ Part 7 - Lists WinForms/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_7___Lists_WinForms
+{
+    public partial class frmThing : Form
+    {
+        public frmThing()
+        {
+            InitializeComponent();
+        }
+    }
+}
